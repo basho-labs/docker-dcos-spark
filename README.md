a9e036a929c2d9247e9c347e43cc6bececad6aa2
Travis B. Hartwell <thartwell@contractor.basho.com>
2016-01-13 13:18:06 -0700

    Install the DCOS CLI packages for Riak, Spark, and Kafka at build time.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This creates a Docker image that installs the DCOS CLI and Spark 1.5.2.
 
+This expects you already have the Spark tarball downloaded from (for example)
+http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.6.tgz in the root
+of the repository.
+
 You'll want to set up an alias because you need to set a few environment variables:
 
     > alias dcoscli="docker run -it -e MESOS_URL=http://127.0.0.1:5050 -e MARATHON_URL=http://127.0.0.1:8080 -e DCOS_URL=http://127.0.0.1 jbrisbin/dcoscli"
--- install_dcos_pkgs.sh
+++ install_dcos_pkgs.sh
@@ -4,5 +4,9 @@ source /opt/dcos-cli/cli/bin/env-setup-dev
 dcos config set core.email "dcos"
 dcos config set core.reporting "false"
 dcos config set package.cache /tmp/dcos
+dcos config set package.sources '["https://github.com/mesosphere/universe/archive/version-1.x.zip"]'
 dcos config prepend package.sources https://github.com/basho-labs/riak-mesos-dcos-repo/archive/0.3.0.zip
 dcos package update
+dcos package install --cli --yes riak
+dcos package install --cli --yes kafka
+dcos package install --cli --yes --package-version="1.5.0-multi-roles-v2" spark
